4aeb3198cc6252cd1d527ccb108eff4303675721
souledspirit <arts9161@gmail.com>
2024-01-17 21:06:35 +0530

    second changes

--- bionite/src/App.js
+++ bionite/src/App.js
@@ -1,21 +1,28 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+// import './App.css';
+// import LoginForm from './LoginForm';
+// import BackgroundImage from './BackgroundImage';
+// import RegistrationForm from './RegistrationForm'; // Create this component
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="authentication-container">
+        <Switch>
+          <Route path="/login" exact component={LoginForm} />
+          <Route path="/register" exact component={RegistrationForm} />
+          <Route path="/" exact component={BackgroundImage} />
+          <Redirect to="/login" />{" "}
+          {/* Redirect to the login page if no route matches */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
